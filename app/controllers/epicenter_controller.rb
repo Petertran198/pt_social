d91d2a5d1efb3ddef14b820ad60d0d9eb95af27c
Peter <petertran198@gmail.com>
2020-05-11 20:44:47 -0400

    Added controller action for feed

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -1,5 +1,13 @@
 class EpicenterController < ApplicationController
   def feed
+    #This include tweeets from people we follow and our own tweets 
+    @following_tweet = []
+    Tweet.all.each do |tweet| 
+      if current_user.following.include?(tweet.user_id) || current_user.id == tweet.user_id 
+        @following_tweet.push(tweet)
+      end
+    end
+
   end
 
   def show_user
