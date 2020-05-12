1d3c8a7115819cdaf8392afa8b14ca8cc5fd0176
Peter <petertran198@gmail.com>
2020-05-11 21:19:53 -0400

    Added following and unfollowing functionality

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -7,15 +7,23 @@ class EpicenterController < ApplicationController
         @following_tweet.push(tweet)
       end
     end
-
   end
 
   def show_user
+    @user = User.find(params[:id])
   end
 
   def now_following
+    #You have to push as an integer because on the show_user page it is searching for if the current_user includes? (@user.id)
+    #@ user.id is an integer 
+    current_user.following.push(params[:id].to_i) #This is yourself 
+    current_user.save
+    redirect_to show_user_path(id: params[:id])
   end
 
   def unfollow
+    current_user.following.delete(params[:id].to_i)
+    current_user.save 
+    redirect_to show_user_path(id: params[:id])
   end
 end
